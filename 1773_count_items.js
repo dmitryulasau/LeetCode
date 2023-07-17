dd8314958714af1b9c6e653a90b615dd90ed888e
dmitryulasau <dmitrushok@gmail.com>
2023-07-17 14:01:58 -0400

    add 1773

---
+++ 1773_count_items.js
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/count-items-matching-a-rule/
+
+var countMatches = function (items, ruleKey, ruleValue) {
+  let count = 0;
+  for (let i = 0; i < items.length; i++) {
+    if (ruleKey === "color") {
+      if (items[i][1] === ruleValue) {
+        count++;
+      }
+    } else if (ruleKey === "type") {
+      if (items[i][0] === ruleValue) {
+        count++;
+      }
+    } else if (ruleKey === "name") {
+      if (items[i][3] === ruleValue) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
+
+console.log(
+  countMatches(
+    [
+      ["phone", "blue", "pixel"],
+      ["computer", "silver", "lenovo"],
+      ["phone", "gold", "iphone"],
+    ],
+    "color",
+    "silver"
+  )
+);
+
+console.log(
+  countMatches(
+    [
+      ["phone", "blue", "pixel"],
+      ["computer", "silver", "lenovo"],
+      ["phone", "gold", "iphone"],
+    ],
+    "type",
+    "phone"
+  )
+);
