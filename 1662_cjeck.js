7bde92b32d942e223f6396e97d10e7f2cf636702
dmitryulasau <dmitrushok@gmail.com>
2023-07-19 21:11:38 -0400

    add 1662

---
+++ 1662_cjeck.js
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+var arrayStringsAreEqual = function (word1, word2) {
+  return word1.join("") === word2.join("");
+};
+
+console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"]));
