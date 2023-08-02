6103e101ad9ef86e053a762b452a819c0341904b
dmitryulasau <dmitrushok@gmail.com>
2023-08-01 21:56:00 -0400

    add 1389

---
+++ 1389_target_array.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+var createTargetArray = function (nums, index) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    result.splice([index[i]], 0, nums[i]);
+  }
+
+  return result;
+};
+console.log(createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
