456a7f390e75754c7013b4d11ff5026ab7961963
dmitryulasau <dmitrushok@gmail.com>
2023-08-08 22:45:54 -0400

    add 2798

---
+++ 2798-.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/number-of-employees-who-met-the-target/
+
+var numberOfEmployeesWhoMetTarget = function (hours, target) {
+  let count = 0;
+
+  for (let i = 0; i < hours.length; i++) {
+    if (hours[i] >= target) {
+      count++;
+    }
+  }
+
+  return count;
+};
--- test.js
+++ test.js
@@ -1,60 +1,9 @@
-// function f(d) {
-//   let p = 0;
-//   let q = d[0];
-
-//   while (p < d.length - 1) {
-//     d[p] = d[p + 1];
-//     p = p + 1;
-//   }
-
-//   if (p === d.length) {
-//     let temp = d[0];
-//     d[0] = d[d.length - 1];
-//     d[d.length - 1] = temp;
-//   }
-
-//   d[d.length - 1] = q;
-
-//   return d;
-// }
-
-// console.log(f([7, 1, 3, 14]));
-
-// function w(n) {
-//   if (n === 1) {
-//     return 1;
-//   }
-
-//   return w(n / 2) + n;
-// }
-
-// console.log(w(4));
-
-function fun(z) {
-  if (z === 0) {
-    return 0;
+function a(n) {
+  for (let i = 0; i < n.length; i++) {
+    let x = "I'm here";
   }
 
-  return bun(z - 1) + 1;
+  return x;
 }
 
-function bun(z) {
-  return 2 * fun(z - 1);
-}
-
-console.log(bun(3));
-
-function jam(a) {
-  let z = a[0];
-  let i = 0;
-  while (i < a.length) {
-    if (z < a[i]) {
-      z = a[i];
-    }
-    i = i + 1;
-  }
-
-  return z;
-}
-
-console.log(jam([1, 2, 3]));
+console.log(a(2));
