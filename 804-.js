69764996e9b1a383e69e592acf54512214f2bc8b
dmitryulasau <dmitrushok@gmail.com>
2023-08-18 21:05:07 -0400

    add 804

---
+++ 804-.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/unique-morse-code-words/
+
+var uniqueMorseRepresentations = function(words) {
+    onst morseCodeMapping = [
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
+        "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+        "-.--", "--.."
+    ];
+    
+    const transformedSet = new Set();
+    
+    for (const word of words) {
+        let transformed = '';
+        for (const char of word) {
+            const index = char.charCodeAt(0) - 'a'.charCodeAt(0);
+            transformed += morseCodeMapping[index];
+        }
+        transformedSet.add(transformed);
+    }
+    
+    return transformedSet.size;
+};
