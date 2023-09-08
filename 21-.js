230db28f4fa6e6de5f9765025ef384fd7131d206
dmitryulasau <dmitrushok@gmail.com>
2023-09-07 20:23:11 -0400

    add 21

---
+++ 21-.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+var mergeTwoLists = function (list1, list2) {
+  let dummy = new ListNode(-1); // Create a dummy node to simplify the code
+  let current = dummy;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  // If one of the lists still has remaining elements, append them to the result
+  if (list1 !== null) {
+    current.next = list1;
+  } else {
+    current.next = list2;
+  }
+
+  return dummy.next; // Return the merged list starting from the next of the dummy node
+};
