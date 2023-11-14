e5cd7a26be6a21e6091fe80cfe5ebec110cd5091
dmitryulasau <dmitrushok@gmail.com>
2023-11-14 12:26:31 -0500

    add 125

---
+++ 125-.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+var isPalindrome = function (s) {
+  // Convert the string to lowercase and remove non-alphanumeric characters
+  const cleanedString = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  // Compare the cleaned string with its reverse
+  return cleanedString === cleanedString.split("").reverse().join("");
+};
